b617f3a8577c442bd19e23eb3366dd3c1737aac4
Chaitanya21-bit <cyrao378@gmail.com>
2022-07-01 13:01:03 +0530

    Added Game

---
+++ utils.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+def scale_image(img, factor):
+    size = round(img.get_width() * factor), round(img.get_height() * factor)
+    return pygame.transform.scale(img, size)
+
+
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(
+        center=image.get_rect(topleft=top_left).center)  # to remove the distortions in the image
+    win.blit(rotated_image, new_rect.topleft)
+
+
+def blit_text_center(win, font, text):
+    render = font.render(text, 1, (200, 200, 200))
+    win.blit(render, (100, 350))
